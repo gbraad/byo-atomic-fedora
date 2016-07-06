9d854f67aa73cd4b33df63ad1155fec78bc0a1b4
Gerard Braad <me@gbraad.nl>
2016-07-06 11:25:26 +0000

    Shallow checkout

--- compose.sh
+++ compose.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 pushd /home/working
-git clone https://git.fedorahosted.org/git/fedora-atomic.git fedora-atomic
 
-cd fedora-atomic
-git checkout f23
+git clone https://git.fedorahosted.org/git/fedora-atomic.git \
+    -b f23 \
+    --depth 1 \
+    fedora-atomic
 
-rpm-ostree compose tree --repo=/srv/repo ./fedora-atomic-docker-host.json
+rpm-ostree compose tree \
+    --repo=/srv/repo \
+    ./fedora-atomic/fedora-atomic-docker-host.json
 
 popd
